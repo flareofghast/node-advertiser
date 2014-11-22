c25a04c9d31fee75494b16b361896ea695619d54
Peter Salzmann <petersalzmann@gmail.com>
2014-11-23 10:20:55 +1030

    add the advertiser

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ node-advertiser
 ===============
 
 Node.js implementation of minecraft advertiser
+
+To run simply type node advert.js
---
+++ advert.js
@@ -0,0 +1,35 @@
+/**
+ *  based on the work from kebian
+ *  http://void7.net/advertising-linux-minecraft-servers-to-the-lan/
+ *  author: FlareOfGhast
+ */
+
+var dgram = require('dgram');
+var socket = dgram.createSocket("udp4");
+var broadcastIP = "255.255.255.255"
+var broadcastPort = 4445
+
+// add your servers like the example separated by commas eg. [MOTD,PORT],[MOTD,PORT] 
+// there is no tailing comma
+var servers = [
+	["A MOTD here", 25565]
+];
+
+// allow broadcast
+socket.bind( function(){
+	socket.setBroadcast(true);
+});
+
+// simple output
+console.log("Broadcasting Minecraft servers to LAN")
+
+//run broadcast method every 1500 ms
+setInterval(broadcast,1500);
+
+function broadcast() {
+	for (var i in servers){
+                var msg =  new Buffer("[MOTD]" + servers[i][0] + "[/MOTD][AD]" + servers[i][1] + "[/AD]");
+                socket.send(msg, 0, msg.length, broadcastPort, broadcastIP, function(err, bytes) {
+		});
+    };
+}
