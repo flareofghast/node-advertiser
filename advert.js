989cbb6540067051a7994b97ffe23955d724742b
Peter Salzmann <petersalzmann@gmail.com>
2014-11-25 17:13:39 +1030

    Updated some comments

--- advert.js
+++ advert.js
@@ -4,10 +4,12 @@
  *  author: FlareOfGhast
  */
 
+var keypress = require('keypress');
 var dgram = require('dgram');
 var socket = dgram.createSocket("udp4");
 var broadcastIP = "255.255.255.255"
 var broadcastPort = 4445
+var child = require('child_process');
 
 // add your servers like the example separated by commas eg. [MOTD,PORT],[MOTD,PORT] 
 // there is no tailing comma
@@ -23,13 +25,16 @@ socket.bind( function(){
 // simple output
 console.log("Broadcasting Minecraft servers to LAN")
 
-//run broadcast method every 1500 ms
+// run broadcast method every 1500 ms
 setInterval(broadcast,1500);
 
+
 function broadcast() {
+	// for each server in servers[] create a new message and send through socket
 	for (var i in servers){
                 var msg =  new Buffer("[MOTD]" + servers[i][0] + "[/MOTD][AD]" + servers[i][1] + "[/AD]");
                 socket.send(msg, 0, msg.length, broadcastPort, broadcastIP, function(err, bytes) {
+                	// call back, launches this code once message is sent
 		});
     };
 }
